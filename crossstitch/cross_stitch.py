563e2077ba0fb583d28994abf598e9c051ee6b47
NPU-Franklin <1109797240@qq.com>
2021-02-24 10:31:57 +0800

    :bug: Fixing a bug.

--- crossstitch/cross_stitch.py
+++ crossstitch/cross_stitch.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 class CrossStitch(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
-        # initialize
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels * 2, in_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(in_channels),
