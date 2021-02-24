c80194be7b2bb5d39da745fd2d9e5ddfa6880c0e
NPU-Franklin <1109797240@qq.com>
2021-02-24 10:31:21 +0800

    Adding cross_stitch matrix

--- crossstitch/cross_stitch.py
+++ crossstitch/cross_stitch.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 class CrossStitch(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
-        # initialize
+        self.cross_stitch = nn.Parameter(torch.tensor([0.5, 0.5]))
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels * 2, in_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(in_channels),
@@ -16,6 +16,8 @@ class CrossStitch(nn.Module):
         )
 
     def forward(self, input1, input2):
+        input1 = input1 * self.cross_stitch[0]
+        input2 = input2 * self.cross_stitch[1]
         output = torch.cat([input1, input2], dim=1)
 
         return self.conv(output)
